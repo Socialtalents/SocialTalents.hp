c66b2fec0e1e09da83049b2cd1f21ff37ec84e98
fiji <fiji@socialtalents.com>
2017-05-27 14:45:12 -0700

    Release version for nuget

--- release.sh
+++ release.sh
@@ -1,1 +1,4 @@
-./nuget push $(pwd)/SocialTalents.Hp.Events/bin/debug/$(ls SocialTalents.Hp.Events/bin/debug/ -t1 |  head -n 1) $(cat ../hp/nugetapi.txt) -Source https://www.nuget.org/api/v2/package
+echo "Going to Publish to nuget:"
+ls SocialTalents.Hp.Events/bin/Release/ -t1 -la |  head -n 2 | tail -n 1
+read -n1 -r -p "Press any key to continue..." key
+./nuget push $(pwd)/SocialTalents.Hp.Events/bin/Release/$(ls SocialTalents.Hp.Events/bin/Release/ -t1 |  head -n 1) $(cat ../hp/nugetapi.txt) -Source https://www.nuget.org/api/v2/package
