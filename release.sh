41cddc803d9e2bcbda8db392d9a09bbf1c76fd69
fiji <fiji@socialtalents.com>
2017-11-03 01:55:23 -0700

    FIxed net standard package; added nuget for MongoDB

--- release.sh
+++ release.sh
@@ -6,3 +6,7 @@ echo "Going to Publish to nuget:"
 ls SocialTalents.Hp.Events.netstandard/bin/Release/ -t1 -la |  head -n 2 | tail -n 1
 read -n1 -r -p "Press any key to continue..." key
 ./nuget push $(pwd)/SocialTalents.Hp.Events.netstandard/bin/Release/$(ls SocialTalents.Hp.Events.netstandard/bin/Release/ -t1 |  head -n 1) $(cat ../hp/nugetapi.txt) -Source https://www.nuget.org/api/v2/package
+
+ls SocialTalents.Hp.MongoDb.netstandard/bin/Release/ -t1 -la |  head -n 2 | tail -n 1
+read -n1 -r -p "Press any key to continue..." key
+./nuget push $(pwd)/SocialTalents.Hp.MongoDb.netstandard/bin/Release/$(ls SocialTalents.Hp.MongoDb.netstandard/bin/Release/ -t1 |  head -n 1) $(cat ../hp/nugetapi.txt) -Source https://www.nuget.org/api/v2/package
