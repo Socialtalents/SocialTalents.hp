620b85effe939110595f09292c84ff648035b175
Sergiy Voshchykov <sergiyv@skyhive.io>
2019-10-07 14:37:09 -0700

    custom Id serializer provider

---
+++ SocialTalents.Hp.MongoDb.net45/IdCustomSerializationProviderHelper.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialTalents.Hp.MongoDB
+{
+    public static class IdCustomSerializationProviderHelper
+    {
+        public static IBsonSerializationProvider RegisterBsonCustomSerializers()
+        {
+            var provider = new IdCustomSerializationProvider();
+            BsonSerializer.RegisterSerializationProvider(provider);
+
+            return provider;
+        }
+    }
+}
